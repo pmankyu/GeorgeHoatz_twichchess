d88ed0aa925face81018a21f4ce47efb4b26b9cc
pmankyu <pmankyu@gmail.com>
2020-02-06 22:03:57 +0900

    ~ 1:44:23

--- state.py
+++ state.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python
 
 import chess
+import numpy as np
 
 class State(object):
-    def __init__(self):
-        self.board = chess.Board()
+    def __init__(self,board=None):
+        if board is None: 
+            self.board = chess.Board()
+        else:
+            self.board = board
 
     def serialize(self):
-        # 257 bits according to readme
-        pass
+        assert self.board.is_valid()
+        bstate = np.zeros(64, np.uint8)
+        for i in range(64):
+            pp = self.board.piece_at(i)
+            if pp is not None:
+                bstate[i] = {"P": 1, "N": 2, "B": 3, "R": 4, "Q": 5, "K": 6 \
+                             "P": 9, "N":10, "B":11, "R":12, "Q":13, "K":14}[pp.symbol()] 
+        if self.board.ep_Square is not None:
+            assert bstate[self.board.ep_Square] == 0
+            bstate[self.board.ep_Square] == 0
+        bstate = bstate.reshape(8,8) 
+        exit(0)
+
+        state = np.zeros((8,8,5), np.uint8)
+        
+        state[:,:,0] = (bstate>>3)&1
+        state[:,:,1] = (bstate>>2)&1
+        state[:,:,2] = (bstate>>1)&1
+        state[:,:,3] = (bstate>>0)&1
+
+        # 4th column is who's turn it is
+        state[:, :, 4] = (self.board.turn*1.0)
+        
+        return state
 
     def edges(self):
         return list(self.board.legal_moves)
--- train.py
+++ train.py
@@ -1,1 +1,26 @@
-sdf
+#!/usr/bin/env python3
+
+import os
+import chess.pgn
+from state import State
+
+# pgn files in this folder
+
+for fn in os.listdir("data"):
+    pgn = open(os.path.join("data",fn))
+    while 1:
+        try:
+            game = chess.pgn.read_game(pgn)
+        except Exception:
+            break
+
+        # result = * (ongoing)
+        value = {'1/2-1/2':0, '0-1':-1, '1-0':1, '*':2}[game.headers['Result']]
+
+        board = game.board()
+        for i, move in enumerate(game.mainline_moves()):
+            board.push(move)
+            # TODO : extract the boards
+            print(value, State(board).serialize())
+        exit(0)
+    break
